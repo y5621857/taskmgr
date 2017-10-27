5949a4c4489cd4ce2d19512d896c6b2aa106b18d
y5621857 <229655153@qq.com>
2017-10-27 22:28:02 +0800

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MdSidenavModule } from '@angular/material'
 
+import { CoreModule } from "./core/core.module"
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { LoginModule } from "./login/login.module";
+import { ProjectModule } from "./project/project.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    MdSidenavModule,
+    AppRoutingModule,
+    LoginModule,
+    ProjectModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+}
