b55b1c38712ebb4c630b5a3f76b399eb974988bf
y5621857 <229655153@qq.com>
2017-11-03 12:52:25 +0800

    学习rxjs高阶，添加注册的身份，地区模块联动，实现数据的管理

--- src/app/login/register/register.component.ts
+++ src/app/login/register/register.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms'
+import { Component, OnInit, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs/Subscription'
+import { isValidDate, toDate } from '../../utils/date.util';
+import { extractInfo, isValidAddr, getAddrByCode } from '../../utils/identity.util';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: [ './register.component.scss' ]
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, OnDestroy {
 
   private items: string[]
   form: FormGroup
-  private readonly avatarName = 'avatars'
+  sub: Subscription
+  readonly avatarName = 'avatars'
 
   constructor( private fb: FormBuilder ) {
   }
@@ -34,6 +38,30 @@ export class RegisterComponent implements OnInit {
       address: [],
       identity: []
     })
+    const id$ = this.form.get('identity').valueChanges
+      .debounceTime(300)
+      .filter(_ => this.form.get('identity').valid)
+
+    this.sub = id$.subscribe(id => {
+      const info = extractInfo(id.identityNo)
+
+      if (isValidAddr(info.addrCode)) {
+        const addr = getAddrByCode(info.addrCode);
+        this.form.patchValue({ address: addr });
+        this.form.updateValueAndValidity({ onlySelf: true, emitEvent: true });
+      }
+      if (isValidDate(info.dateOfBirth)) {
+        const date = info.dateOfBirth;
+        this.form.patchValue({ dateOfBirth: date });
+        this.form.updateValueAndValidity({ onlySelf: true, emitEvent: true });
+      }
+    })
+  }
+
+  ngOnDestroy() {
+    if (this.sub) {
+      this.sub.unsubscribe()
+    }
   }
 
   /**
--- src/app/utils/identity.util.ts
+++ src/app/utils/identity.util.ts
@@ -1,29 +1,43 @@
 import { GB2260 } from './identity.data'
 
-export function extractInfo(idNo: string) {
+/**
+ * 获取身份信息
+ * @param {string} idNo
+ * @returns {{addrCode: string; dateOfBirth: string; gender: boolean}}
+ */
+export function extractInfo( idNo: string ) {
   const addrPart = idNo.substring(0, 6); // 前六位地址码
   const birthPart = idNo.substring(6, 14); // 八位生日
   const genderPart = parseInt(idNo.substring(14, 17), 10); // 性别
   return {
     addrCode: addrPart,
     dateOfBirth: birthPart,
-    gender: genderPart % 2 !== 0
+    gender: genderPart%2 !== 0
   };
 }
 
-export function isValidAddr(code: string): boolean {
-  return GB2260[code] !== undefined;
+/**
+ * 验证身份证地址合法性
+ * @param {string} code
+ * @returns {boolean}
+ */
+export function isValidAddr( code: string ): boolean {
+  return GB2260[ code ] !== undefined;
 }
 
-export const getAddrByCode = (code) => {
-  const provinceStr = GB2260[code.substring(0, 2) + '0000'];
-  const cityStr = GB2260[code.substring(0, 4) + '00'];
-  const districtStr = GB2260[code];
-  const city = cityStr.replace(provinceStr, '');
-  const district = districtStr.replace(cityStr, '');
+/**
+ * 获得地址信息
+ * @param code
+ * @returns {{province: any; city: (string | any | void); district: (string | any | void)}}
+ */
+export const getAddrByCode = ( code ) => {
+  const provinceStr = GB2260[ code.substring(0, 2) + '0000' ]; //省
+  const cityStr = GB2260[ code.substring(0, 4) + '00' ].replace(provinceStr, ''); //市
+  const districtStr = GB2260[ code ].replace(provinceStr + cityStr, ''); //区
+
   return {
     province: provinceStr,
-    city: city,
-    district: district
+    city: cityStr,
+    district: districtStr
   };
 };
